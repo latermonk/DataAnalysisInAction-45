26fac3289feec35c20167422860ebe8a4b2ec2b3
latermonk <iback.china@gmail.com>
2020-01-25 19:03:30 +0800

    Create 03_loop.py

---
+++ 03/03_loop.py
@@ -0,0 +1,15 @@
+#  for in
+sum = 0
+for number in range(11):
+    sum = sum + number
+print (sum)
+
+
+
+#  while
+sum = 0
+number = 1
+while number < 11:
+       sum = sum + number
+       number = number + 1
+print (sum)
